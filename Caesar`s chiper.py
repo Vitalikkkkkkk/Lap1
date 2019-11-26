0e3f62f96705892a55f9aa9a71dd2e428994e606
Vitalya <56473990+Vitalikkkkkkk@users.noreply.github.com>
2019-11-26 18:35:24 +0200

    Add files via upload

---
+++ Caesar`s chiper.py
@@ -0,0 +1,37 @@
+alph = ("abcdefghijklmnopqrstuvwsyzabcdefghijklmnopqrstuvwsyz")
+num = ("1234567890")
+encr = input("Enter a massage: ")
+encr = encr + " "
+key = 1
+i = 0
+while i < 1:
+    i += 1
+    encr2 = ""
+    a = ""
+    for letter in encr:
+        position = alph.find(letter)
+        pos = num.find(letter)
+        newPosititon2 = pos + key
+        newPosititon = position + key
+        if letter in alph:
+            if a != "":
+                a = int(a)
+                a = a + key
+                a = str(a)
+                encr2 = encr2 + a
+                a = ""
+
+            encr2 = encr2 + alph[newPosititon]
+        elif letter in num:
+            a = a + letter
+        else:
+            if a != "":
+                a = int(a)
+                a = a + key
+                a = str(a)
+                encr2 = encr2 + a
+                a = ""
+            encr2 = encr2 + letter
+
+    print("Your cipher is: ", encr2)
+
